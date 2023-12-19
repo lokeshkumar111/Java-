189566794d374e278821ff13a060cb82bc7d52a1
lokeshkumar111 <lokeshk.charan3@gmail.com>
2023-12-19 18:02:16 +0530

    rotate the array k times in the right direction

---
+++ DSA/Easy/rotateArray.java
@@ -0,0 +1,57 @@
+/*
+ Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 105
+ */
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        rotate(nums, k, n);
+    }
+
+    public static void rotate(int[] nums, int k, int n) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = nums[(i + k + 1) % n];
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+
+        }
+
+        System.out.println();
+    }
+}
