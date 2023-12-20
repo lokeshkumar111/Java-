4d1b314694494dc501db354cb2b78b8fdedb2031
lokeshkumar111 <lokeshk.charan3@gmail.com>
2023-12-20 14:50:40 +0530

    move zeros to the right

---
+++ DSA/Easy/moveZerotoTheLeft.java
@@ -0,0 +1,51 @@
+/* 
+ Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+*/
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        moveZeroes(n, nums);
+    }
+
+    public static void moveZeroes(int n, int[] nums) {
+        int nonZeroPointer = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[nonZeroPointer];
+                nums[nonZeroPointer] = nums[i];
+                nums[i] = temp;
+                nonZeroPointer++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
