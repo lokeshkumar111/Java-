5fbedeae8e4a24a7c8dbf6a65556f133b69d98dc
lokeshkumar111 <lokeshk.charan3@gmail.com>
2024-01-11 19:18:53 +0530

    updated

--- DSA/Easy/largestElementInArray.java
+++ DSA/Easy/largestElementInArray.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-class Main {
+class largestElementInArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); // Length of the array
