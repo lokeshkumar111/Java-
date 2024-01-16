050ef6096dd9adb6275ae5597324f14087304061
lokeshkumar111 <lokeshk.charan3@gmail.com>
2024-01-16 14:10:19 +0530

    left side right angle triangle

--- DSA/Pattern/Pattern1.java
+++ DSA/Pattern/Pattern1.java
@@ -12,6 +12,41 @@ n=6
 
  */
 
+// package DSA.Pattern;
+
+// import java.util.*;
+
+// public class Pattern1 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for (int i = n - 1; i > 0; i--) {
+//             for (int j = 0; j < i; j++) {
+//                 System.out.print("* ");
+//             }
+//             System.out.println();
+//         }
+//     }
+
+// }
+
+// ------------------------------------------------------------------------------------------------------------------------------
+
+/*                                              --------------------
+                                                    Pattern : 2
+                                                --------------------
+print the following pattern :- 
+
+n=6
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * * * 
+
+ */
+
 package DSA.Pattern;
 
 import java.util.*;
@@ -20,13 +55,11 @@ public class Pattern1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = n - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
-        sc.close();
     }
-
 }
