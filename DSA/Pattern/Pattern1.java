9e6d5b68754fa8bfc4277c1cf30dd76914d0eec5
lokeshkumar111 <lokeshk.charan3@gmail.com>
2024-01-16 13:29:32 +0530

    pattern

---
+++ DSA/Pattern/Pattern1.java
@@ -0,0 +1,32 @@
+/*                                              --------------------
+                                                    Pattern : 1
+                                                --------------------
+print the following pattern :- 
+
+n=6
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+
+ */
+
+package DSA.Pattern;
+
+import java.util.*;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+}
