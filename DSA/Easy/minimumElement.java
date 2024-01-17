97adb0cb05f6a930641bf225da1431595b09cd6c
lokeshkumar111 <lokeshk.charan3@gmail.com>
2024-01-17 18:00:55 +0530

    smallest element

---
+++ DSA/Easy/minimumElement.java
@@ -0,0 +1,22 @@
+// We have to find the smallest element in the the array. 
+
+import java.util.Scanner;
+
+public class minimumElement {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // Length of the array
+        int[] arr = new int[n]; // array of the length n
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt(); // fill the values in the array
+        }
+        int min = Integer.MAX_VALUE; // taking a minimum value
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < min) {
+                min = arr[i]; // after each iteration it modifies the value of the min.
+            }
+        }
+        System.out.println(min); // It return the minimun value
+    }
+}
