d630ca242d6bea71f176c45485ec5fa69a849551
lokeshkumar111 <lokeshk.charan3@gmail.com>
2024-01-27 18:12:10 +0530

    changes done

--- DSA/Pattern/Pattern1.java
+++ DSA/Pattern/Pattern1.java
@@ -1,15 +1,16 @@
-/*                                              --------------------
-                                                    Pattern : 1
-                                                --------------------
-print the following pattern :- 
-
-n=6
-* * * * * 
-* * * * 
-* * * 
-* * 
-*
-
+/*
+ * --------------------
+ * Pattern : 1
+ * --------------------
+ * print the following pattern :-
+ * 
+ * n=6
+ * * * * *
+ * * * *
+ * * *
+ * *
+ *
+ * 
  */
 
 // package DSA.Pattern;
@@ -17,49 +18,88 @@ n=6
 // import java.util.*;
 
 // public class Pattern1 {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         int n = sc.nextInt();
-//         for (int i = n - 1; i > 0; i--) {
-//             for (int j = 0; j < i; j++) {
-//                 System.out.print("* ");
-//             }
-//             System.out.println();
-//         }
-//     }
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int n = sc.nextInt();
+// for (int i = n - 1; i > 0; i--) {
+// for (int j = 0; j < i; j++) {
+// System.out.print("* ");
+// }
+// System.out.println();
+// }
+// }
 
 // }
 
 // ------------------------------------------------------------------------------------------------------------------------------
 
-/*                                              --------------------
-                                                    Pattern : 2
-                                                --------------------
-print the following pattern :- 
-
-n=6
-* 
-* * 
-* * * 
-* * * * 
-* * * * * 
-* * * * * * 
-
+/*
+ * --------------------
+ * Pattern : 2
+ * --------------------
+ * print the following pattern :-
+ * 
+ * n=6
+ * 
+ * *
+ * * *
+ * * * *
+ * * * * *
+ * * * * * *
+ * 
  */
 
-package DSA.Pattern;
+// package DSA.Pattern;
 
-import java.util.*;
+// import java.util.*;
 
-public class Pattern1 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-    }
-}
+// public class Pattern1 {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int n = sc.nextInt();
+// for (int i = 0; i < n; i++) {
+// for (int j = 0; j <= i; j++) {
+// System.out.print("* ");
+// }
+// System.out.println();
+// }
+// }
+// }
+
+// -------------------------------------------------------------------------------------------------------
+
+/*
+ * --------------------
+ * Pattern : 3
+ * --------------------
+ * print the following pattern :-
+ * 
+ * n=6
+ * 
+ * 
+ * 
+ * 
+ * * * * *
+ * 
+ */
+
+// package DSA.Pattern;
+
+// import java.util.*;
+
+// public class Pattern1 {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int n = sc.nextInt();
+// for (int i = 0; i < n; i++) {
+// for (int j = 0; j <= i; j++) {
+// if (j < n - 1) {
+// System.out.print("* ");
+// } else {
+// System.out.print("* ");
+// }
+// }
+// System.out.println();
+// }
+// }
+// }
