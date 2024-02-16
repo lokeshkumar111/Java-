84e20baeaf968b5b2fe9741efd0459ca68111bae
lokeshkumar111 <lokeshk.charan3@gmail.com>
2024-02-16 12:13:19 +0530

    updated

--- DSA/Easy/factorial.java
+++ DSA/Easy/factorial.java
@@ -13,13 +13,13 @@ class factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(fact(n));
+        System.out.println(factor(n));
     }
 
-    public static int fact(int n) {
+    public static int factor(int n) {
         if (n == 0 || n == 1) {
             return 1;
         }
-        return n * fact(n - 1);
+        return n * factor(n - 1);
     }
 }
