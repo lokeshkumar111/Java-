801bb27a51ba321754bd99593540d85d38e63322
lokeshkumar111 <lokeshk.charan3@gmail.com>
2024-02-20 14:29:39 +0530

    sum of n numbers

---
+++ DSA/Easy/findSumOfNNumber.java
@@ -0,0 +1,64 @@
+
+/*
+
+Problem statement
+You are given an integer ’n’.
+
+
+
+Your task is to return a sorted array (in increasing order) containing all the factorial numbers which are less than or equal to ‘n’.
+
+
+
+The factorial number is a factorial of a positive integer, like 24 is a factorial number, as it is a factorial of 4.
+
+
+
+Note:
+In the output, you will see the array returned by you.
+Example:
+Input: ‘n’ = 7
+
+Output: 1 2 6
+
+Explanation: Factorial numbers less than or equal to ‘7’ are ‘1’, ‘2’, and ‘6’.
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1:
+7
+Sample Output 1 :
+1 2 6
+Explanation Of Sample Input 1:
+Input: ‘n’ = 7
+
+Output: 1 2 6
+
+Explanation: Factorial numbers less than or equal to ‘7’ are ‘1’, ‘2’, and ‘6’.
+Sample Input 2:
+2
+Sample Output 2:
+1 2
+Explanation Of Sample Input 2:
+Input: ‘n’ = 2
+
+Output: 1 2
+
+Explanation: Factorial numbers less than or equal to ‘2’ are ‘1’ and ‘2’.
+
+*/
+import java.util.*;
+
+public class findSumOfNNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(sum(n));
+    }
+
+    public static int sum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n + sum(n - 1);
+
+    }
+}
