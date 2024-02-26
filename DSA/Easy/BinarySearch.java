a2cd2e126e4df187467643edbd46829527b9a42a
lokeshkumar111 <lokeshk.charan3@gmail.com>
2024-02-26 17:36:42 +0530

    updated

--- DSA/Easy/BinarySearch.java
+++ DSA/Easy/BinarySearch.java
@@ -27,8 +27,9 @@ nums is sorted in ascending order.
 
 class BinarySearch {
     public int search(int[] nums, int target) {
+        int n = nums.length;
         int left = 0;
-        int right = nums.length - 1;
+        int right = n;
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
